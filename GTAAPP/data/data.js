8f6ea2859bd77b98871c7fe921d0d5b5a35897d5
SxntX1623 <127440542+SxntX1623@users.noreply.github.com>
2023-10-19 16:27:03 -0500

    Add files via upload

---
+++ GTAAPP/data/data.js
@@ -0,0 +1,39 @@
+export const data =
+    [
+        {
+            "id": 1,
+            "name": "CJ",
+            "image": "./IMG/CJ.png"
+        },
+
+        {
+            "id": 2,
+            "name": "SWEET",
+            "image": "/IMG/SWEET.webp"
+        },
+
+        {
+            "id": 3,
+            "name": "RYDER",
+            "image": "/IMG/RYDER.webp"
+        },
+
+        {
+            "id": 4,
+            "name": "BIGSMOOK",
+            "image": "/IMG/BigSmoke.webp"
+        },
+
+        {
+            "id": 5,
+            "name": "TENPENNY",
+            "image": "/IMG/tenpenny.png"
+        },
+
+        {
+            "id": 6,
+            "name": "THE BALLAS",
+            "image": "/IMG/Ballas.jpg"
+        },
+
+    ]
